68e1d79d2842a91bbbaa66f937cf45fb410f9e23
niktyugay <niktyugay@localhost>
2019-10-15 14:27:47 +0300

    Создал отдельные библиотеки для работы с шаговыми двигателями и дровами

---
+++ pumps/inc/motor.h
@@ -0,0 +1,13 @@
+/*
+ * motor.h
+ *
+ *  Created on: 15 окт. 2019 г.
+ *      Author: niktyugay
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+#include "stm32f10x.h"
+
+#endif /* MOTOR_H_ */
---
+++ pumps/src/motor.c
@@ -0,0 +1,9 @@
+/*
+ * motor.c
+ *
+ *  Created on: 15 окт. 2019 г.
+ *      Author: niktyugay
+ */
+
+#include "motor.h"
+
